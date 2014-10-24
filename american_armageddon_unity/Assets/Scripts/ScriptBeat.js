621374e03ee108e47ea7bde3b20bef687c646f4f
gamebreakingbug <mediaapplied@medias-imac-2.home>
2014-10-24 12:24:15 +0100

    stable

---
+++ american_armageddon_unity/Assets/Scripts/ScriptBeat.js
@@ -0,0 +1,237 @@
+﻿#pragma strict
+
+    public class StoryCharacter {
+
+        var _cn : String; // name
+        var _pp : String; // pronoun
+        var _pn : String; // possessive
+        var _on : String; // opposition character
+        var _ff : String; // faction
+        var _nn : String; // nemesis
+        var _dn : String[]; // domains
+
+        function StoryCharacter(  cn : String,
+                                  pp : String,
+                                  pn : String,
+                                  on : String,
+                                  ff : String,
+                                  nn : String,
+                                  dn : String[] ) {
+
+            this._cn = cn;
+            this._pp = pp;
+            this._pn = pn;
+            this._on = on;
+            this._ff = ff;
+            this._nn = nn;
+            this._dn = dn;
+        }
+
+    }
+
+    public class ScriptLine {
+
+        var _text : String;
+        var _type : String;
+        var _role : String;
+
+        function ScriptLine(  text : String,
+                              type : String,
+                              role : String ) {
+
+            this._text = text;
+            this._type = type;
+            this._role = role;
+
+        }
+
+    }
+
+    public class ScriptBeat extends MonoBehaviour {
+
+        var scriptCharacters : StoryCharacter[];
+        var twineTitles : String[];
+        var twinePassages : Dictionary.<String,TwinePassage>;
+
+        function LoadData() {
+
+            // create new characters and store in an array
+            var duke : StoryCharacter = new StoryCharacter("Duke","he","his","Herzog","the group","the militia",["generic","justice","chaos"]);
+            var herzog : StoryCharacter = new StoryCharacter("Herzog","he","his","Duke","his congregation","the group",["generic","gain","order"]);
+            var maggie : StoryCharacter = new StoryCharacter("Maggie","she","her","Bubonic Jil","the group","the undead",["generic","justice","order"]);
+            var jil : StoryCharacter = new StoryCharacter("Bubonic Jil","she","her","Duke","the militia","the congregation",["generic","gain","chaos"]);
+
+            scriptCharacters = [duke, herzog, maggie, jil];
+
+            // get Twine passages from TwineManager
+            twinePassages = gameObject.GetComponent(TwineManager).GetPassages();
+            twineTitles = gameObject.GetComponent(TwineManager).GetTitles();
+
+            for(var i : int = 0;i < 200; i++){
+
+                Debug.Log(StoryLine());
+                Debug.Log(FlavourText());
+            }
+
+        }
+
+        function FindTitle(title:String) : boolean {
+
+            if(System.Array.IndexOf(twineTitles, title) == -1){
+                Debug.Log("broken link" + title);
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+
+        function StoryLine() : String {
+
+            // POV Character
+            var pov : StoryCharacter;
+            // story text
+            var storyLine : String;
+            // temp string
+            var buffer : StringBuilder = new StringBuilder();
+            // get random POV character
+            var rnd : int = Random.Range(0,scriptCharacters.Length);
+            pov = scriptCharacters[rnd];
+            var name : String;
+            var space : String = " ";
+
+            // decide if a prefix will be added
+            var prefixList : String[] = ["prefixes","short_prefixes", null];
+            var getPrefixType : String = prefixList[Random.Range(0,prefixList.Length)];
+
+            if(getPrefixType != null && FindTitle(getPrefixType)) {
+                var prefixType : TwinePassage = twinePassages[getPrefixType];
+                // select from list of link passages
+                var prefixLink : String = prefixType.GetRandomLink();
+                if(prefixLink != null && FindTitle(getPrefixType)) {
+                    var prefix = twinePassages[prefixLink];
+                    // add prefix text to buffer
+                    buffer.Append(prefix.title);
+                    buffer.Append(space);
+                }
+            }
+
+            // add POV character name
+            buffer.Append(pov._cn);
+            buffer.Append(space);
+
+            //select verb domain
+            var getVerbType : String = pov._dn[Random.Range(0,pov._dn.Length)];
+            var verbType = twinePassages[getVerbType];
+            // select from list of link passages
+            var verbLink : String = verbType.GetRandomLink();
+            if(verbLink != null && FindTitle(verbLink)) {
+                var verb : TwinePassage = twinePassages[verbLink];
+                // add verb text to buffer
+                buffer.Append(verb.title);
+                buffer.Append(space);
+            }
+            else {
+                // if function breaks re-start
+                return StoryLine();
+            }
+
+            // select story beat
+            var beatLink : String = verb.GetRandomLink();
+            if(beatLink != null && FindTitle(beatLink)) {
+                var beat : TwinePassage = twinePassages[beatLink];
+                // add verb text to buffer
+                buffer.Append(beat.title);
+                buffer.Append(".");
+            }
+            else {
+                // if function breaks re-start
+                return StoryLine();
+            }
+
+            var txt : String = buffer.ToString();
+
+            // replace placeholder text
+            txt = txt.Replace("%PP",pov._pp);
+            txt = txt.Replace("%PN",pov._pn);
+            txt = txt.Replace("%ON",pov._on);
+            txt = txt.Replace("%FF",pov._ff);
+            txt = txt.Replace("%NN",pov._nn);
+
+            // capitalise first letter
+            var capital : String = txt.Substring(0,1);
+            txt = txt.Substring(1,txt.Length-1);
+            capital = capital.ToUpper();
+            txt = capital+txt;
+
+            return txt;
+
+        }
+
+        function FlavourText() : String {
+
+            var flavour_EE : String[] = [         "the swarm",
+                                                  "the undead horde",
+                                                  "a murderous mob",
+                                                  "a direful throng",
+                                                  "a savage flock",
+                                                  "a dark force",
+                                                  "an ominous terror",
+                                                  "the herd" ];
+
+            var flavour_VV : String[] = [         "sinister lies",
+                                                  "evil horrors",
+                                                  "terrible nightmares",
+                                                  "painful terrors",
+                                                  "fearful truths" ];
+
+            var buffer : StringBuilder = new StringBuilder();
+            var space : String = " ";
+
+            var flavourType : TwinePassage = twinePassages["flavourPfx"];
+            var flavourLink : String = flavourType.GetRandomLink();
+            if(flavourLink != null) {
+                var flavour : TwinePassage = twinePassages[flavourLink];
+                // add flavour text to buffer
+                buffer.Append(flavour.title);
+                buffer.Append(space);
+            }
+            else {
+                // if function breaks re-start
+                return FlavourText();
+            }
+
+            var textType : TwinePassage = twinePassages["flavourSfx"];
+
+            var textLink : String = textType.GetRandomLink();
+            if(textLink != null) {
+                var text : TwinePassage = twinePassages[textLink];
+                // add text to buffer
+                buffer.Append(text.title);
+                buffer.Append(".");
+            }
+            else {
+                // if function breaks re-start
+                return FlavourText();
+            }
+
+            var txt : String = buffer.ToString();
+
+            // replace placeholder text
+            txt = txt.Replace("%EE",flavour_EE[Random.Range(0,flavour_EE.Length)]);
+            txt = txt.Replace("%VV",flavour_VV[Random.Range(0,flavour_VV.Length)]);
+
+            // capitalise first letter
+            var capital : String = txt.Substring(0,1);
+            txt = txt.Substring(1,txt.Length-1);
+            capital = capital.ToUpper();
+            txt = capital+txt;
+
+            return txt;
+
+
+        }
+
+
+
+    }
---
+++ american_armageddon_unity/Assets/Scripts/TwineManager.js
@@ -0,0 +1,184 @@
+#pragma strict
+
+import System.Collections.Generic;
+import System.Text;
+
+    public class TwinePassage {
+
+        var title : String;
+        var tags : String[];
+        var body : String;
+        var links : String[];
+
+        function CheckTag(txt:String){
+
+            for(var i: int = 0; i < tags.Length; i++){
+                if(txt == tags[i]){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function FindLinks(){
+
+            // check if passage is of type domain or verb
+            if(CheckTag("domain")||CheckTag("verb")){
+
+                // body text will consist of "[[some link]]" so split string based on lines
+                links = body.Split("\n"[0]);
+
+                // trim "[[" and "]]" from each link
+
+                var text : String;
+                var index : int;
+                for (var i : int = 0; i < links.Length; i++){
+
+                    text = links[i];
+                    index = text.Length;
+                    if(index > 0){
+                        text = text.Substring(2,index-4);
+                        links[i] = text;
+                    }
+                }
+            }
+
+        }
+
+        function GetRandomLink() {
+
+            var linkTitle : String;
+            if(links.Length > 0){
+                var r : int = Random.Range(0,links.Length);
+                linkTitle = links[r];
+                if(linkTitle.length > 0){
+                    return linkTitle;
+                }
+                else{
+                    return null;
+                }
+            }
+            else{
+                return null;
+            }
+
+
+        }
+
+        function DebugPassage() {
+
+            Debug.Log(title);
+            for(var i : int = 0; i < tags.Length; i++){
+                Debug.Log(tags[i]);
+            }
+            Debug.Log(body);
+            for(var j : int = 0; j < links.Length; j++){
+                Debug.Log(links[j]);
+            }
+        }
+
+    }
+
+    public class TwineManager extends MonoBehaviour {
+
+        // the TextAsset resource for parsing
+        public var twineSourceAsset : TextAsset;
+
+        // the Dictionary to hold all the Twine passages, keyed by title
+        var passages = new Dictionary.<String,TwinePassage>();
+
+        // a reference array of the dictionary keys
+        var titles : String[] = new String[0];
+
+        // the string that will contain the unformatted Twine file
+        private var twineSource : String;
+
+        function Parse() {
+
+            // reference to the current passage being built from source
+            var currentPassage : TwinePassage = null;
+
+            // buffer to hold the content of current passage while building it
+            var buffer : StringBuilder = new StringBuilder();
+
+            // array that will hold all the individual lines in the Twine source
+            var lines : String[];
+
+            // utility array used when splitting a string
+            var chunks : String[];
+
+            // split the Twine source into lines
+            lines = twineSource.Split("\n"[0]);
+
+            // iterating through entire file
+            for (var i : int = 0; i < lines.Length; i++){
+
+                // if a line begins with "::" then a new passage has started
+                if(lines[i].StartsWith("::")){
+
+                    // if we were already building a passage, that one is now complete
+                    // wrap it up and add it to the dictionary
+                    if(currentPassage != null){
+                        currentPassage.body = buffer.ToString();
+                        currentPassage.FindLinks();
+                        passages.Add(currentPassage.title, currentPassage);
+                        buffer = new StringBuilder();
+                    }
+
+                    // 2 represents ignoring the "::" prefix, then splits the line at "["
+                    chunks = lines[i].Substring(2).Replace("]","").Split("["[0]);
+
+                    // start a new passage with passage title
+                    currentPassage = new TwinePassage();
+                    currentPassage.title = chunks[0].Trim();
+
+                    // if there is anything after "[" the passage has tags, so these are split and then added
+                    if(chunks.Length > 1){
+                        currentPassage.tags = chunks[1].Trim().Split(" "[0]);
+                    }
+
+                } else if(currentPassage != null){
+
+                    //if didn't start a new passage we are still in previous passage, so append line to current passage buffer
+                    buffer.AppendLine(lines[i]);
+                }
+            }
+
+            // at end of loop wrap up last passage and end
+            if (currentPassage != null){
+                currentPassage.body = buffer.ToString();
+                currentPassage.FindLinks();
+                passages.Add(currentPassage.title, currentPassage);
+            }
+
+        }
+
+        function GetPassages() : Dictionary.<String,TwinePassage> {
+
+            return passages;
+
+        }
+
+        function GetTitles() : String[] {
+
+            return titles;
+
+        }
+
+        function Start() {
+
+            // Load the twine source file from resources and store the text as a huge String
+            //twineSourceAsset = Resources.Load("storybeats") as TextAsset;
+            twineSource = twineSourceAsset.text;
+
+            // Parse the data
+            Parse();
+
+            // Populate the reference array
+            titles = new String[passages.Count];
+            passages.Keys.CopyTo(titles, 0);
+
+            gameObject.GetComponent(ScriptBeat).LoadData();
+        }
+
+    }
